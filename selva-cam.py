a813370a9cf854214737ade11644d78a8f49405b
Luis G. López <lopezsolerluis@gmail.com>
2023-02-24 17:00:16 -0300

    Otro cambio drástico

--- selva-cam.py
+++ selva-cam.py
@@ -11,94 +11,72 @@ margen_foto = 3
 alto_base = 3
 alto_cubre = alto_base
 alto_papel = 1
-borde_agarre = 3.5
+espesor_garra = 3.5
 bisel = 1
 espesor_abrazadera = 2
-holgura = .3
+holgura = .25
 
 largo_base = 2*lado_foto+4*borde_foto
+ancho_base_foto = lado_foto+2*borde_foto+2*espesor_garra
+ancho_base_cubre = lado_foto+2*borde_foto
 xAgujero = lado_foto/2+borde_foto
 
-def base(alto,ancho_agarre):
-    ancho_base = lado_foto+2*borde_foto+2*ancho_agarre
-    yMuesca = ancho_base/2-ancho_agarre
+def base(alto,ancho):
     return(cq.Workplane()
-             .rect(largo_base,ancho_base)
+             .rect(largo_base,ancho)
              .extrude(alto)
              .edges("|X")
              .chamfer(bisel)
-             .faces("<X")
-             .workplane()
-             .center(yMuesca,0)
-             .polyline([(bisel,0),(-bisel,0),(0,bisel)])
-             .close()
-             .cutThruAll()
-             .center(-2*yMuesca,0) # This redundancy is awful!
-             .polyline([(bisel,0),(-bisel,0),(0,bisel)])
-             .close()
-             .cutThruAll()
              .copyWorkplane(cq.Workplane())
              .pushPoints([(xAgujero,0),(-xAgujero,0)])
              .rect(lado_foto,lado_foto)
              .cutThruAll()
            )
 
-def base_foto(ancho_agarre):
+def base_foto():
     lado_margen = lado_foto+margen_foto*2
-    return (base(alto_base,ancho_agarre)
+    return (base(alto_base,ancho_base_foto)
             .faces(">Z")
             .workplane()
             .pushPoints([(xAgujero,0),(-xAgujero,0)])
             .rect(lado_margen,lado_margen)
             .cutBlind(-alto_papel)
+            # garras
+            .copyWorkplane(cq.Workplane("YZ"))
+            .center(0,alto_base)
+            .polyline([(-ancho_base_foto/2,-bisel),
+                       (-ancho_base_foto/2,alto_cubre+holgura),
+                       (-ancho_base_foto/2+bisel,alto_cubre+bisel+holgura),
+                       (-ancho_base_foto/2+espesor_garra,alto_cubre+bisel+holgura),
+                       (-ancho_base_foto/2+espesor_garra+bisel-holgura,alto_cubre+holgura),
+                       (-ancho_base_foto/2+espesor_garra-holgura,alto_cubre+holgura-bisel),
+                       (-ancho_base_foto/2+espesor_garra-holgura,0)
+                       ])
+            .close()
+            .mirrorY()
+            .extrude(largo_base/2,both=True)
             )
 
-def cubre(translucido,ancho_agarre):
-    return (base(alto_cubre,ancho_agarre)
+def cubre(translucido):
+    return (base(alto_cubre,ancho_base_cubre)
             .copyWorkplane(cq.Workplane("YZ"))
             .split(keepTop=translucido, keepBottom=not translucido)
             )
 
-def abrazadera():
-    return (cq.Workplane("YZ")
-            .polyline([(-holgura,0),(-holgura,alto_cubre+holgura),
-                       (borde_agarre-bisel,alto_cubre+holgura),
-                       (borde_agarre,alto_cubre-bisel+holgura),
-                       (borde_agarre+bisel,alto_cubre+holgura),
-                       (borde_agarre+2*bisel,alto_cubre+holgura),
-                       (borde_agarre+2*bisel,alto_cubre+espesor_abrazadera+holgura-bisel/2),
-                       (borde_agarre+2*bisel-bisel/2,alto_cubre+espesor_abrazadera+holgura),
-                       (-holgura-bisel,alto_cubre+espesor_abrazadera+holgura),
-                       (-espesor_abrazadera-holgura,alto_cubre+holgura+bisel),
-                       (-espesor_abrazadera-holgura,0)
-                       ])
-            .mirrorX()
-            .extrude(largo_base/2)
-            )
-
 asm = cq.Assembly()
 
-asm.add(base_foto(borde_agarre), name="base", color=cq.Color("red"))
-asm.add(cubre(translucido=True,ancho_agarre=borde_agarre), name="cubre_translucido",
+asm.add(base_foto(), name="base", color=cq.Color("red"))
+asm.add(cubre(translucido=True), name="cubre_translucido",
         color=cq.Color("green"),
         loc=cq.Location((0,0,0),(1,0,0),180)*
             cq.Location(cq.Vector(0,0,-(alto_base+alto_cubre))))
-asm.add(cubre(translucido=False,ancho_agarre=borde_agarre), name="cubre_fotografico",
+asm.add(cubre(translucido=False), name="cubre_fotografico",
         color=cq.Color("cyan"),
         loc=cq.Location((0,0,0),(1,0,0),180)*
             cq.Location(cq.Vector(0,0,-(alto_base+alto_cubre))))
 
-ancho_base = lado_foto+2*borde_foto+2*borde_agarre
-asm.add(abrazadera(), name="abrazadera_1", color=cq.Color("blue"),
-        loc=cq.Location(cq.Vector(0,-ancho_base/2,alto_base)))
-asm.add(abrazadera(), name="abrazadera_2", color=cq.Color("blue"),
-        loc=cq.Location(cq.Vector(0,ancho_base/2,alto_base))*
-            cq.Location((0,0,0),(1,0,0),180))
-asm.add(abrazadera(), name="abrazadera_3", color=cq.Color("orange"),
-        loc=cq.Location(cq.Vector(-largo_base/2,-ancho_base/2,alto_base)))
-asm.add(abrazadera(), name="abrazadera_4", color=cq.Color("orange"),
-        loc=cq.Location(cq.Vector(-largo_base/2,ancho_base/2,alto_base))*
-            cq.Location((0,0,0),(1,0,0),180))
+
+
 #asm.add(cortina(), name="cortina", color=cq.Color("blue"), loc=cq.Location(cq.Vector(-.25*borde_foto,0,a_placa_foto*2+alto_agarre/2)))
 #asm.add(cuerpo(), name="cuerpo", color=cq.Color("orange"), loc=cq.Location(cq.Vector((largo_cuerpo/2-borde_foto/2),0,alto_cuerpo/2-a_placa_foto)))
 
