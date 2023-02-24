f05701d92b15053f863b6908cc50d1c448af429d
Luis G. López <lopezsolerluis@gmail.com>
2023-02-24 18:37:02 -0300

    Primera versión de cortina

--- selva-cam.py
+++ selva-cam.py
@@ -21,6 +21,7 @@ ancho_muesca = 10
 largo_base = 2*lado_foto+4*borde_foto
 ancho_base_foto = lado_foto+2*borde_foto+2*espesor_garra
 ancho_base_cubre = lado_foto+2*borde_foto
+ancho_cortina = ancho_base_cubre - 4*bisel - 2*espesor_garra
 xAgujero = lado_foto/2+borde_foto
 
 def base(alto,ancho):
@@ -99,6 +100,14 @@ def cubre():
             .extrude(largo_base/4,both=True)
             )
 
+def cortina():
+    return (cq.Workplane()
+            .rect(largo_base/2,ancho_cortina)
+            .extrude(alto_cortina)
+            .edges("|X")
+            .chamfer(bisel)
+            )
+
 extra_alto=0
 asm = cq.Assembly()
 
@@ -109,7 +118,9 @@ asm.add(cubre(), name="cubre_translucido",
 asm.add(cubre(), name="cubre_fotografico",
         color=cq.Color("cyan"),
         loc=cq.Location(cq.Vector(-largo_base/2,0,alto_base+extra_alto)))
-
+asm.add(cortina(), name="cortina",
+        color=cq.Color("blue"),
+        loc=cq.Location(cq.Vector(-largo_base/4,0,alto_base+alto_cubre+2*extra_alto)))
 
 
 #asm.add(cortina(), name="cortina", color=cq.Color("blue"), loc=cq.Location(cq.Vector(-.25*borde_foto,0,a_placa_foto*2+alto_agarre/2)))
