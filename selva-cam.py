e1b332f4093dc8caab6de966cc0eced0ba52b43c
Luis G. López <lopezsolerluis@gmail.com>
2023-02-24 21:05:30 -0300

    Cambios en la caja

--- selva-cam.py
+++ selva-cam.py
@@ -18,14 +18,16 @@ espesor_abrazadera = 2
 holgura_justa = .2
 holgura_movil = .3
 ancho_muesca = 10
-borde_caja = 5
+borde_caja = 8
 ancho_traba = 2
+margen_agujeritos = 5
 
 largo_base = 2*lado_foto+4*borde_foto
 ancho_base_foto = lado_foto+2*borde_foto+2*espesor_garra
 ancho_base_cubre = lado_foto+2*borde_foto
 ancho_cortina = ancho_base_cubre - 4*bisel - 2*espesor_garra
 xAgujero = lado_foto/2+borde_foto
+largo_caja = largo_base/2
 ancho_caja = ancho_base_foto + 2*borde_caja
 alto_caja = 2*borde_caja + alto_base + alto_cubre + alto_cortina + 2*bisel + holgura_movil
 largo_cortina = largo_base/2+borde_foto-ancho_traba
@@ -137,17 +139,16 @@ def cortina():
 
 def caja():
     return (cq.Workplane()
-            .rect(largo_base/2,ancho_caja)
+            .rect(largo_caja,ancho_caja)
             .extrude(alto_caja)
             .chamfer(bisel)
             .copyWorkplane(cq.Workplane("YZ"))
             .center(0,borde_caja)
             .polyline([(0,0),
                        (ancho_base_foto/2+holgura_movil,0),
-                       (ancho_base_foto/2+holgura_movil,alto_base+alto_cubre+bisel+holgura_justa+holgura_movil),
-                       (ancho_base_foto/2+holgura_movil-espesor_garra,alto_base+alto_cubre+bisel+holgura_justa+holgura_movil),
-                       (ancho_base_foto/2+holgura_movil-espesor_garra-2*bisel,alto_base+alto_cubre+bisel+holgura_justa+holgura_movil),
-                       (ancho_base_foto/2+holgura_movil-espesor_garra-2*bisel,alto_base+alto_cubre+bisel+2*holgura_movil-bisel+alto_cortina+2*bisel),
+                       (ancho_base_foto/2+holgura_movil,alto_base+alto_cubre+holgura_justa+holgura_movil),
+                       #(ancho_base_foto/2+holgura_movil-espesor_garra-2*bisel,alto_base+alto_cubre+bisel+holgura_justa+holgura_movil),
+                       (ancho_base_foto/2-espesor_garra-2*bisel,alto_base+alto_cubre+bisel+2*holgura_movil-bisel+alto_cortina+2*bisel),
                        (0,alto_base+alto_cubre+bisel+2*holgura_movil-bisel+alto_cortina+2*bisel),
                        ])
             .mirrorY()
@@ -160,6 +161,12 @@ def caja():
             .faces(">Z")
             .circle(d_exterior_acople/2-espesor_acople)
             .cutBlind('next')
+            # agujeritos
+            .faces("<Z")
+            .rect(largo_caja-2*margen_agujeritos,ancho_caja-2*margen_agujeritos, forConstruction=True)
+            .vertices()
+            .circle(1.75/2+.1)
+            .cutThruAll()
             )
 
 extra_alto=0
